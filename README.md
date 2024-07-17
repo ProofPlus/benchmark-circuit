50837faa9701104b202e8d9d9df433423ef29794
Mihail Kirov <mihail.kirov@limechain.tech>
2024-07-17 16:20:53 +0300

    feat: benchmark circuit

--- README.md
+++ README.md
@@ -1,40 +1,15 @@
-# RISC Zero Foundry Template
+# RISC Zero Benchmark circuit
 
-> **Note: This software is not production ready. Do not use in production.**
-
-> Note: This template was recently updated to work without the relay.
-> If you'd like the relay version, head over to the [relay branch](https://github.com/risc0/bonsai-foundry-template/tree/relay)
-
-Starter template for writing an application using [RISC Zero] and Ethereum.
-
-This repository implements an application on Ethereum utilizing RISC Zero as a [coprocessor] to the smart contract application.
-It provides a starting point for building powerful new applications on Ethereum that offload computationally intensive (i.e. gas expensive), or would be difficult to implement Solidity (e.g. ed25519 signature verification, or HTML parsing).
-
-<!-- TODO(#100) Integrate support for Steel more directly into this repo -->
-Integrate with [Steel][steel-repo] to execute view calls and simulate transactions on Ethereum. Check out the [ERC-20 counter][erc20-counter] demo to see an example.
-
-Prove computation with the [RISC Zero zkVM] and verify the results in your Ethereum contract.
-
-## Overview
-
-Here is a simplified overview of how devs can integrate RISC Zero, with [Bonsai] proving, into their Ethereum smart contracts:
-
-![RISC Zero Foundry Template Diagram](images/risc0-foundry-template.png)
-
-1. Run your application logic in the [RISC Zero zkVM]. The provided [publisher] app sends an off-chain proof request to the [Bonsai] proving service.
-2. [Bonsai] generates the program result, written to the [journal], and a SNARK proof of its correctness.
-3. The [publisher] app submits this proof and journal on-chain to your app contract for validation.
-4. Your app contract calls the [RISC Zero Verifier] to validate the proof. If the verification is successful, the journal is deemed trustworthy and can be safely used.
+# Overview
+This is a repo containing the Risc Zero code used to benchmark proving time for your prover hardware. The circuit contains a single ED25519 signature verification, which is exactly 1_256_081 cycles. The proof generation time in seconds is later used by the ProofPlus protocol to set each individual task's deadline and to calculate the task rewards.
 
 ## Dependencies
 
-First, [install Rust] and [Foundry], and then restart your terminal.
+First, [install Rust] and then restart your terminal.
 
 ```sh
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh
-# Install Foundry
-curl -L https://foundry.paradigm.xyz | bash
 ```
 
 Next, you will need to install the `cargo risczero` tool.
@@ -61,116 +36,20 @@ git submodule update --init --recursive
 
 ### Build the Code
 
-- Builds for zkVM program, the publisher app, and any other Rust code.
+- Builds for zkVM benchmark program
 
   ```sh
   cargo build
   ```
 
-- Build your Solidity smart contracts
+### Run local benchmark
 
-  > NOTE: `cargo build` needs to run first to generate the `ImageID.sol` contract.
-
-  ```sh
-  forge build
+ ```sh
+  time cargo test
   ```
 
-### Run the Tests
-
-- Tests your zkVM program.
-
-  ```sh
-  cargo test
-  ```
-
-- Test your Solidity contracts, integrated with your zkVM program.
-
-  ```sh
-  RISC0_DEV_MODE=true forge test -vvv 
-  ```
-
-### Configuring Bonsai
-
-***Note:*** *To request an API key [complete the form here](https://bonsai.xyz/apply).*
-
-With the Bonsai proving service, you can produce a [Groth16 SNARK proof] that is verifiable on-chain.
-You can get started by setting the following environment variables with your API key and associated URL.
-
-```bash
-export BONSAI_API_KEY="YOUR_API_KEY" # see form linked above
-export BONSAI_API_URL="BONSAI_URL" # provided with your api key
-```
-
-Now if you run `forge test` with `RISC0_DEV_MODE=false`, the test will run as before, but will additionally use the fully verifying `RiscZeroGroth16Verifier` contract instead of `MockRiscZeroVerifier` and will request a SNARK receipt from Bonsai.
-
-```bash
-RISC0_DEV_MODE=false forge test -vvv
-```
-
-### Deterministic Builds
-
-By setting the environment variable `RISC0_USE_DOCKER` a containerized build process via Docker will ensure that all builds of your guest code, regardless of the machine or local environment, will produce the same [image ID][image-id].
-The [image ID][image-id], and its importance to security, is explained in more detail in our [developer FAQ].
-
-```bash
-RISC0_USE_DOCKER=1 cargo build
-```
-
-> ***Note:*** *This requires having Docker installed and in your PATH. To install Docker see [Get Docker].*
-
-## Deploy Your Application
-
-When you're ready, follow the [deployment guide] to get your application running on [Sepolia].
-
-## Project Structure
-
-Below are the primary files in the project directory
-
-```text
-.
-├── Cargo.toml                      // Configuration for Cargo and Rust
-├── foundry.toml                    // Configuration for Foundry
-├── apps
-│   ├── Cargo.toml
-│   └── src
-│       └── lib.rs                  // Utility functions
-│       └── bin                     
-│           └── publisher.rs        // Example app to publish program results into your app contract 
-├── contracts
-│   ├── EvenNumber.sol              // Basic example contract for you to modify
-│   └── ImageID.sol                 // Generated contract with the image ID for your zkVM program
-├── methods
-│   ├── Cargo.toml
-│   ├── guest
-│   │   ├── Cargo.toml
-│   │   └── src
-│   │       └── bin                 // You can add additional guest programs to this folder
-│   │           └── is_even.rs      // Example guest program for checking if a number is even
-│   └── src
-│       └── lib.rs                  // Compiled image IDs and tests for your guest programs
-└── tests
-    ├── EvenNumber.t.sol            // Tests for the basic example contract
-    └── Elf.sol                     // Generated contract with paths the guest program ELF files.
-```
-
-[Bonsai]: https://dev.bonsai.xyz/
-[Foundry]: https://getfoundry.sh/
-[Get Docker]: https://docs.docker.com/get-docker/
-[Groth16 SNARK proof]: https://www.risczero.com/news/on-chain-verification
-[RISC Zero Verifier]: https://github.com/risc0/risc0/blob/release-0.21/bonsai/ethereum/contracts/IRiscZeroVerifier.sol
 [RISC Zero installation]: https://dev.risczero.com/api/zkvm/install
 [RISC Zero zkVM]: https://dev.risczero.com/zkvm
 [RISC Zero]: https://www.risczero.com/
-[Sepolia]: https://www.alchemy.com/overviews/sepolia-testnet
-[app contract]: ./contracts/
 [cargo-binstall]: https://github.com/cargo-bins/cargo-binstall#cargo-binaryinstall
-[coprocessor]: https://www.risczero.com/news/a-guide-to-zk-coprocessors-for-scalability
-[deployment guide]: /deployment-guide.md
-[developer FAQ]: https://dev.risczero.com/faq#zkvm-application-design
-[image-id]: https://dev.risczero.com/terminology#image-id
 [install Rust]: https://doc.rust-lang.org/cargo/getting-started/installation.html
-[journal]: https://dev.risczero.com/terminology#journal
-[publisher]: ./apps/README.md
-[zkVM program]: ./methods/guest/
-[steel-repo]: https://github.com/risc0/risc0-ethereum/tree/main/steel
-[erc20-counter]: https://github.com/risc0/risc0-ethereum/tree/main/examples/erc20-counter
--- methods/src/lib.rs
+++ methods/src/lib.rs
@@ -3,29 +3,11 @@ include!(concat!(env!("OUT_DIR"), "/methods.rs"));
 #[cfg(test)]
 mod tests {
 
-    use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
-    use risc0_zkvm::serde::to_vec;
-    use bytemuck::cast_slice;
-
-    use risc0_zkvm::sha::Digestible;
-    use risc0_zkvm::compute_image_id;
-
+    use risc0_zkvm::{default_prover, ExecutorEnv};
     use sha2::{Digest, Sha384};
 
     use ed25519_dalek::{Signature, Signer, SigningKey};
     use rand::rngs::OsRng;
-
-    use std::io::Write;
-    use anyhow::{Context, Result};
-    use ethers::abi::Token;
-
-    use risc0_ethereum_contracts::groth16::Seal;
-    use alloy_primitives::FixedBytes;
-    use std::time::Duration;
-
-    use bonsai_sdk::alpha as bonsai_sdk;
-
-    use crate::{MAIN_ELF, MAIN_ID};
 
     fn compute_merkle_root(leaf: &Vec<u8>, merkle_path: &Vec<Vec<u8>>) -> Vec<u8> {
         let mut current_hash: Vec<u8> = Sha384::digest(leaf).to_vec();
@@ -43,108 +25,6 @@ mod tests {
         }
     
         current_hash
-    }
-
-    fn run_bonsai(signature_input: ([u8; 32], Vec<u8>, Vec<u8>), leaf_hash: Vec<u8>, merkle_path: Vec<Vec<u8>> ) -> Result<(Vec<u8>, FixedBytes<32>, Vec<u8>), anyhow::Error> {
-        let client = bonsai_sdk::Client::from_env(risc0_zkvm::VERSION)?;
-    
-        // Compute the image_id, then upload the ELF with the image_id as its key.
-        // let image_id = hex::encode(compute_image_id(MAIN_ELF)?);
-        let image_id = compute_image_id(MAIN_ELF).unwrap().to_string();
-        tracing::info!("image_id: {}", image_id);
-        client.upload_img(&image_id, MAIN_ELF.to_vec())?;
-    
-        // Prepare input data and upload it.
-        // let input_data = to_vec(&signature_input).unwrap();
-        let mut input_data: Vec<u8> = Vec::new();
-        input_data.extend_from_slice(cast_slice(&to_vec(&signature_input)?));
-        input_data.extend_from_slice(cast_slice(&to_vec(&leaf_hash)?));
-        input_data.extend_from_slice(cast_slice(&to_vec(&merkle_path)?));
-
-        let input_id = client.upload_input(input_data)?;
-        tracing::info!("uploaded input");
-        // Add a list of assumptions
-        let assumptions: Vec<String> = vec![];
-    
-        // Start a session running the prover
-        let session = client.create_session(image_id, input_id, assumptions)?;
-        loop {
-            let res = session.status(&client)?;
-            if res.status == "RUNNING" {
-                tracing::info!(
-                    "Current status: {} - state: {} - continue polling...",
-                    res.status,
-                    res.state.unwrap_or_default()
-                );
-                std::thread::sleep(Duration::from_secs(15));
-                continue;
-            }
-            if res.status == "SUCCEEDED" {
-                // Download the receipt, containing the output
-                let receipt_url = res
-                    .receipt_url
-                    .expect("API error, missing receipt on completed session");
-    
-                let receipt_buf = client.download(&receipt_url)?;
-                let receipt: Receipt = bincode::deserialize(&receipt_buf)?;
-                receipt
-                    .verify(MAIN_ID)
-                    .expect("Receipt verification failed");
-            } else {
-                panic!(
-                    "Workflow exited: {} - | err: {}",
-                    res.status,
-                    res.error_msg.unwrap_or_default()
-                );
-            }
-    
-            break;
-        }
-    
-        // Optionally run stark2snark
-        tracing::info!("Running stark2snark...");
-
-        Ok(run_stark2snark(session.uuid)?)
-    }
-    
-    fn run_stark2snark(session_id: String) -> Result<(Vec<u8>, FixedBytes<32>, Vec<u8>), anyhow::Error>{
-        let client = bonsai_sdk::Client::from_env(risc0_zkvm::VERSION)?;
-    
-        let snark_session = client.create_snark(session_id)?;
-        tracing::info!("Created snark session: {}", snark_session.uuid);
-        let snark_receipt = loop {
-            let res = snark_session.status(&client)?;
-            match res.status.as_str() {
-                "RUNNING" => {
-                    tracing::info!("Current status: {} - continue polling...", res.status,);
-                    std::thread::sleep(Duration::from_secs(15));
-                    continue;
-                }
-                "SUCCEEDED" => {
-                    break res.output.context("No snark generated :(")?;
-                }
-                _ => {
-                    panic!(
-                        "Workflow exited: {} err: {}",
-                        res.status,
-                        res.error_msg.unwrap_or_default()
-                    );
-                }
-            }
-        };
-
-        let snark = snark_receipt.snark;
-        tracing::debug!("Snark proof!: {snark:?}");
-
-        let seal = Seal::abi_encode(snark).context("Read seal")?;
-        let post_state_digest: FixedBytes<32> = snark_receipt
-            .post_state_digest
-            .as_slice()
-            .try_into()
-            .context("Read post_state_digest")?;
-        let journal = snark_receipt.journal;
-
-        Ok((journal, post_state_digest, seal))
     }
 
     #[test]
@@ -166,9 +46,9 @@ mod tests {
 
         let computed_root: Vec<u8> = compute_merkle_root(&leaf_hash,  &merkle_path);
 
-        tracing::info!("computed_root: {:x?}", computed_root);
-        tracing::info!("leaf_hash: {:x?}", leaf_hash);
-        tracing::info!("merkle_path: {:x?}", merkle_path);
+        // tracing::info!("computed_root: {:x?}", computed_root);
+        // tracing::info!("leaf_hash: {:x?}", leaf_hash);
+        // tracing::info!("merkle_path: {:x?}", merkle_path);
 
         let mut csprng = OsRng {};
         let keypair: SigningKey = SigningKey::generate(&mut csprng);
@@ -180,36 +60,18 @@ mod tests {
         let signature_input: ([u8; 32], Vec<u8>, Vec<u8>) = (vk.to_bytes(), computed_root.clone(), signature.to_vec());
         println!("{:?}", signature_input);
 
-        // let env: ExecutorEnv = ExecutorEnv::builder()
-        //     .write(&signature_input)
-        //     .unwrap()
-        //     .write(&leaf_hash)
-        //     .unwrap()
-        //     .write(&merkle_path)
-        //     .unwrap()
-        //     .build()
-        //     .unwrap();
+        let env: ExecutorEnv = ExecutorEnv::builder()
+            .write(&signature_input)
+            .unwrap()
+            .write(&leaf_hash)
+            .unwrap()
+            .write(&merkle_path)
+            .unwrap()
+            .build()
+            .unwrap();
 
-        // let prover = default_prover();
-        // let receipt = prover.prove(env, super::MAIN_ELF).unwrap();
-
-        // receipt.verify(super::MAIN_ID).unwrap();
-
-        let (journal, post_state_digest, seal) =  run_bonsai(signature_input, leaf_hash, merkle_path)?;
-        
-        let calldata = vec![
-            Token::Bytes(journal),
-            Token::FixedBytes(post_state_digest.to_vec()),
-            Token::Bytes(seal),
-        ];
-        let output = hex::encode(ethers::abi::encode(&calldata));
-
-        // Forge test FFI calls expect hex encoded bytes sent to stdout
-        print!("{output}");
-        std::io::stdout()
-            .flush()
-            .context("failed to flush stdout buffer")?;
-
+        let prover = default_prover();
+        prover.prove(env, super::MAIN_ELF).unwrap();
 
         Ok(())
 
